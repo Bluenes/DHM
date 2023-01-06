59876f675ceaa050f6b9d525b5f569507e111f92
Lukee <88560297+Bluenes@users.noreply.github.com>
2023-01-06 23:22:40 +0100

    Update ANTI AA.lua

--- ANTI AA.lua
+++ ANTI AA.lua
@@ -13,41 +13,40 @@ local aimlock = AIMLOCKS:NewSection("AA")
 
 
 aimlock:NewButton("ANTI LOCK keybind{Y}", "ButtonInfo", function()
-    local Toggled = true
-    local KeyCode = 'y'
-    local hip = 2.00
-    local val = -55
+local Toggled = true
+local KeyCode = 'y'
+local hip = 2.00
+local val = -55
+
+
     
+ 
+ function AA()
+    local oldVelocity = game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity
+    game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(oldVelocity.X, val, oldVelocity.Z)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(oldVelocity.X, oldVelocity.Y, oldVelocity.Z)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(oldVelocity.X, val, oldVelocity.Z)
+    game.Players.LocalPlayer.Character.Humanoid.HipHeight = hip
+end
     
+game:GetService('UserInputService').InputBegan:Connect(function(Key)
+    if Key.KeyCode == Enum.KeyCode[KeyCode:upper()] and not game:GetService('UserInputService'):GetFocusedTextBox() then
+        if Toggled then
+            Toggled = false
+            game.Players.LocalPlayer.Character.Humanoid.HipHeight = hip
     
+        elseif not Toggled then
+            Toggled = true
     
-    
-    function AA()
-        local oldVelocity = game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity
-        game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(oldVelocity.X, val, oldVelocity.Z)
-        game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(oldVelocity.X, oldVelocity.Y, oldVelocity.Z)
-        game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(oldVelocity.X, val, oldVelocity.Z)
-        game.Players.LocalPlayer.Character.Humanoid.HipHeight = hip
-    end
-    
-    game:GetService('UserInputService').InputBegan:Connect(function(Key)
-        if Key.KeyCode == Enum.KeyCode[KeyCode:upper()] and not game:GetService('UserInputService'):GetFocusedTextBox() then
-            if Toggled then
-                Toggled = false
-                game.Players.LocalPlayer.Character.Humanoid.HipHeight = hip
-    
-            elseif not Toggled then
-                Toggled = true
-    
-                while Toggled do
-                    AA()
-                    task.wait()
-                end
+            while Toggled do
+                AA()
+                task.wait()
             end
         end
-    end)
-    end)
+    end
+end)
+end)
 
 aimlock:NewKeybind("Toggle GUI", Enum.KeyCode.LeftAlt, function()
     Library:ToggleUI(LeftAlt)
-    end)
+end)
